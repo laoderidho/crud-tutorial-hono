514ae4a61d6cc01827c97fee6ab7b45bb2f6d5a0
laoderidho <ridhofahreza25@gmail.com>
2025-05-01 20:05:58 +0700

    up

--- src/query/Admin/data/dataQuery.ts
+++ src/query/Admin/data/dataQuery.ts
@@ -1,6 +1,6 @@
 
 export function getdataUser(id: number){
-    return `select u.id, u.email, u.name, u.no_telp, r.name as roleName 
+    return `select u.roleId, u.id, u.email, u.name, u.no_telp, r.name as roleName 
             from user u join role r on u.roleId = r.id where u.id != ${id} limit 300`
 }
 
